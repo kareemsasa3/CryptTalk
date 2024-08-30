4aae61d18515ee523a5c281210ba1d999bf79b54
Kareem Sasa <kareemsasa3@gmail.com>
2024-08-30 09:49:38 -0500

    Initial commit with Xcode project setup

---
+++ CryptTalk/CryptTalk/CryptTalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptTalkApp.swift
+//  CryptTalk
+//
+//  Created by Kareem Sasa on 8/30/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptTalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
